dd9cc3f05b06f7e3dc75e2a446c884f29b2df8d5
Devon Black <db126337@student.wpsok.org>
2025-08-20 14:08:11 -0500

    Here's what I did: Added HW2

---
+++ HW2.py
@@ -0,0 +1,29 @@
+#name : Devon Black
+#Class : 6th Hour
+#Assignment: HW1
+
+#1. Print "Hello World!"
+print("Hello World")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+integer_variable = 4
+string_variable = "Oklahoma"
+boolean_variable = True
+#3. Print all three variables on the same print function (at the same time).
+print(integer_variable ,string_variable ,boolean_variable)
+#4. Create a variable that asks the user to input an integer.
+number = int(input("Enter a number: "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+Z = ( integer_variable + number )
+print(Z)
+#6. Take the result from #5 and divide it by 2. Print the result.
+Key = (Z / 2)
+print(Key)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+boolean_variable = False
+#8. Print the value of the boolean variable.
+print (boolean_variable)
+#9. Create a variable with a number that contains decimals.
+Trains = 3.3
+#10. Round the number from #9 up or down using the round function.
+print(round(Trains))
