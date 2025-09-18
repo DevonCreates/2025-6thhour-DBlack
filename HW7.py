8d2e7d1009497dfc47e87524f2dfe3a400d7ca4b
Devon Black <db126337@student.wpsok.org>
2025-09-18 14:09:19 -0500

    Created Playground

---
+++ HW7.py
@@ -0,0 +1,25 @@
+#Name: Devon Black
+#Class: 6th Hour
+#Assignment: HW7
+
+#1. Print Hello World!
+print("Hello World")
+#2. Create a dictionary with 3 keys and a value for each key. One of the keys must have a value with a list containing
+#three numbers inside.
+
+#3. Print the keys of the dictionary from #2.
+
+#4. Print the values of the dictionary from #2
+
+#5. Print one of the three numbers from the list by itself
+
+#6. Using the update function, add a fourth key to the dictionary and give it a value.
+
+#7. Print the entire dictionary from #2 with the updated key and value.
+
+#8. Make a nested dictionary with three entries containing the name of another classmate and two other pieces of information
+#within each entry.
+
+#9. Print the names of all three classmates on the same line.
+
+#10. Use the pop function to remove one of the nested dictionaries inside and print the full dictionary from #8.
---
+++ Playground.py
@@ -0,0 +1,17 @@
+#Name: Devon Black
+#Class: 6th Hour
+#Assignment: Playground
+import random
+
+print("Hello World")
+num_list = [1,2,3,4,5,6,7,8,9]
+random.shuffle(num_list)
+print(num_list)
+num_list.pop(4)
+print(num_list)
+num_list.insert(0,10)
+print(num_list)
+anum_list = [1,2,3,4,5,6,7,8,9]
+print(anum_list[1] + num_list[3])
+people_list = ["Aaden", "Kiah", "Carson", "Annie", "lakyn", "Kate"]
+people_list.
