f8071a38b6b3ff657a85db1c3da651c6679e19a8
Devon Black <db126337@student.wpsok.org>
2025-10-06 13:54:32 -0500

    Created HW8

---
+++ HW9.py
@@ -0,0 +1,16 @@
+#Name:Devon Black
+#Class: 6th Hour
+#Assignment: HW9
+import random
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. Create a list with three variables that each randomly generate a number between 1 and 100
+d1 = random.randint(1,100)
+#3. Print the list.
+
+#4. Create an if statement that determines which of the three numbers is the highest and prints the result.
+
+#5. Tie the result (the largest number) from #4 to a variable called "num".
+
+#6. Create a nested if statement that prints if num is divisible by 2, divisible by 3, both, or neither.
