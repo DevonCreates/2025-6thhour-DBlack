15f4480894ded0de381b93064d73f9fe8758703b
Devon Black <db126337@student.wpsok.org>
2025-10-23 14:09:10 -0500

    Created HW11

---
+++ HW11.py
@@ -0,0 +1,27 @@
+#Name:Devon Black
+#Class: 6th Hour
+#Assignment: HW11
+
+#1. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
+
+#import random library
+import random
+#Create a temperature variable, give it a value (random from 1 to 30).
+Temp_var = random.randint(1, 30)
+print(Temp_var)
+#Make an if statement to see if temperature variable is above 20
+#   - If yes, print it's hot
+#   - If no, bring it to next if statement
+if Temp_var > 20:
+    print("its hot")
+elif Temp_var > 10:
+    print("its mild")
+#Make an if statement to see if temperature variable is above 10
+#   - If yes, print it's mild
+#   - If no, print it's cold
+if Temp_var < 10:
+    print("it's cold")
+#Print "Thank you!" and end the code
+print("Thank you!")
