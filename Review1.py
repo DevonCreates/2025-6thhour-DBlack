e6551a06b2ee5372910a1ab4a8630d42c787da64
Devon Black <db126337@student.wpsok.org>
2026-03-23 13:50:30 -0500

    Finished Review 1

---
+++ Review1.py
@@ -0,0 +1,34 @@
+#Name:Devon Black
+#Class: 6th Hour
+#Assignment: HW-R1
+#1. Print "Hello World!"
+print('Hello world')
+#2. Create three variables with strings as values.
+x = "monster"
+y = "Doors"
+z = "Shirts"
+#3. Create three variables with booleans as values.
+a = True
+b = False
+c = True
+#4. Create three variables with integers as values.
+d = 3
+e = 234
+f = 958
+#5. Create a variable that asks for the user's name and prints it.
+name = str(input("What is your name?: "))
+#6. Add together the three variables from #4 and print the result.
+su = d + e + f
+print(su)
+#7. Subtract the second variable from the first from #4 and print the result.
+ab = d - d
+print(ab)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+ac = f * su
+#9. Create a variable that asks the user for a number with a decimal (a float).
+ba = float(input("Enter a number with a decimal:  "))
+#10. Round the variable from #9 to the nearest whole number and print the result.
+bb = round(ba)
+print(bb)
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+print(355/113)
