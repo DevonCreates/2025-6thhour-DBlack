6e0ac57de4be9b5f1b2e526aa1fc4fe3fa41352b
Devon Black <db126337@student.wpsok.org>
2026-04-06 13:46:27 -0500

    Finished Review 5

---
+++ Review 5.py
@@ -0,0 +1,15 @@
+#Name:Devon Black
+#Class: 6th Hour
+#Assignment: HW-R5
+
+#1. Fix the "class" comment at the top to "6th Hour"
+#2. Create a list of the names of all the students in the classroom.
+class_names = ['Shane', 'Greg', 'Ally', 'tristan', 'Connor', 'ethan', 'Devon ', 'Alaya']
+#3. Create a for loop that prints the names of every student in the list.
+for class_name in class_names:
+    print(class_name)
+#4. Using the "in" operator (hint: Google), create a for loop that only prints
+#the name of a student if the letter "e" is in it.
+for class_names in class_names:
+    if 'e' in class_names:
+        print(class_names)
